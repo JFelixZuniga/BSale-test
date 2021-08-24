04a9a65816b8af3eca0ced7fc49b0264fb6d279c
JFelixZuniga <johnfzuniga@gmail.com>
2021-08-23 20:43:24 -0400

    Front terminado. Manejo de errores agregado

---
+++ README.md
@@ -0,0 +1,35 @@
+## Ejercicio
+
+Ejercicio Construir una tienda online que despliegue productos agrupados por la categoría a la que pertenecen, generando por separado backend (API REST) y frontend (aplicación que la consuma).
+
+Además, hay que agregar un buscador, el cual tiene que estar implementado a nivel de servidor, mediante una Api Rest cuyo lenguaje y framework puede ser de libre elección. Es decir, los datos de productos deben llegar filtrados al cliente.
+
+### BACK
+
+Paquetes utilizados:
+
+- Express
+- MySql
+- Cors
+
+El servidor disponibiliza las siguientes rutas:
+
+- /Api/Products/ GET: Retorna todos los productos.
+- /Api/Products/Category/:category GET: Recibe por parámetro un string y retorna los productos de dicha categoría
+- /Api/Products/Search/:products GET: recibe por parámetro un string y retorna los productos y/o categorías que contenga el string.
+
+### FRONT
+
+Recursos externos utilizados:
+
+- Bootstrap
+- JQuery
+- Font Awesome
+
+  1.- Al acceder al front, una vez construido el DOM se dispara el evento DOMContentLoaded para cargar las primeras funciones del main.js
+  2.- Utiliza API Fetch para el envío de la request del usuario.
+  3.- Para escuchar el evento de agregar productos al carrito después de crear el documento utilizamos el Event Delegation (delegación de eventos).
+
+<h1 align="center">
+   <img src="./captura.png">
+</h1>
--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,31 @@
-console.log("Hola!");
-
 document.addEventListener("DOMContentLoaded", async () => {
   const dataAllProducts = await getAllProducts();
-
   await paintProductCards(dataAllProducts);
+
+  const storage = JSON.parse(localStorage.getItem("carrito"));
+  if (storage) {
+    carrito = storage;
+    renderCarrito();
+  }
 });
 
+const getAllProductsMenu = async () => {
+  try {
+    const dataAllProducts = await getAllProducts();
+    await paintProductCards(dataAllProducts);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
 const getAllProducts = async () => {
-  const response = await fetch("http://localhost:3000/Api/Products/");
-  const data = await response.json();
-  console.log(data);
-  return data;
+  try {
+    const response = await fetch("http://localhost:3000/Api/Products/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 };
 
 const paintProductCards = async (dataProducts) => {
@@ -20,7 +35,13 @@ const paintProductCards = async (dataProducts) => {
 
   dataProducts.productos.forEach(({ name, url_image, price, discount }) => {
     const divCard = document.createElement("div");
-    divCard.classList.add("col-md-3", "col-sm-6", "mb-4", "product-card");
+    divCard.classList.add(
+      "col-lg-3",
+      "col-md-4",
+      "col-sm-6",
+      "mb-4",
+      "product-card"
+    );
     divCard.innerHTML += `
         <div class="card h-100">
           <div class="product-grid">
@@ -47,12 +68,14 @@ const paintProductCards = async (dataProducts) => {
               <h3 class="title"><a href="#">${name}</a></h3>
               ${
                 discount > 0
-                  ? `<div class="price">$${((100 - discount) * price) / 100}
-                    <span>$${price}</span>
+                  ? `<div>
+                    <span class="price">$${((100 - discount) * price) / 100}
+                    </span>
+                    <span class="ex-price">$${price}</span>
                   </div>`
                   : `<div class="price">$${price}</div>`
               }
-                <a class="add-to-cart" href="">+ Agregar</a>
+                <button class="add-to-cart btn btn-outline-warning">+ Agregar</button>
             </div>
           </div>
         </div>
@@ -63,10 +86,163 @@ const paintProductCards = async (dataProducts) => {
 
 const searchProduct = async () => {
   let inputSearch = document.querySelector("#input_search").value;
-  const response = await fetch(
-    `http://localhost:3000/Api/Products/Search/${inputSearch}`
-  );
-  const productsFound = await response.json();
-  console.log(productsFound);
-  await paintProductCards(productsFound);
+  try {
+    const response = await fetch(
+      `http://localhost:3000/Api/Products/Search/${inputSearch}`
+    );
+    const productsFound = await response.json();
+    await paintProductCards(productsFound);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+const getCategory = async (category) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/Api/Products/Category/${category}`
+    );
+    const categoryFound = await response.json();
+    await paintProductCards(categoryFound);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+/**************** Shopping Cart ****************/
+let carrito = [];
+const tbody = document.querySelector(".tbody");
+const btnPagar = document.querySelector(".btn-pagar");
+
+document.addEventListener("click", (event) => {
+  if (event.target && event.target.className.includes("add-to-cart")) {
+    addToCarritoItem(event);
+  } else if (event.target && event.target.className.includes("delete")) {
+    removeItemCarrito(event);
+  }
+});
+
+const addToCarritoItem = (event) => {
+  const button = event.target;
+  const item = button.closest(".card");
+
+  const itemTitle = item.querySelector(".title").textContent;
+  const itemPrice = item.querySelector(".price").textContent.split("$")[1];
+  const itemImg = item.querySelector(".pic-1").src;
+
+  const newItem = {
+    title: itemTitle,
+    price: itemPrice,
+    img: itemImg,
+    cantidad: 1,
+  };
+
+  addItemCarrito(newItem);
+};
+
+const addItemCarrito = (newItem) => {
+  const alert = document.querySelector(".alert");
+
+  setTimeout(function () {
+    alert.classList.add("hide");
+  }, 1000);
+  alert.classList.remove("hide");
+
+  const InputElemnto = tbody.getElementsByClassName("input-elemento");
+  for (let i = 0; i < carrito.length; i++) {
+    if (carrito[i].title.trim() === newItem.title.trim()) {
+      carrito[i].cantidad++;
+      const inputValue = InputElemnto[i];
+      inputValue.value++;
+      CarritoTotal();
+      return null;
+    }
+  }
+
+  carrito.push(newItem);
+
+  renderCarrito();
+};
+
+const renderCarrito = () => {
+  tbody.innerHTML = "";
+  carrito.map((item) => {
+    const tr = document.createElement("tr");
+    tr.classList.add("ItemCarrito");
+    tr.innerHTML += `
+      <td>
+        <img src="${item.img}" alt="bebida" width="50" />
+      </td>
+      <td class="title">${item.title}</td>
+      <td>${item.price}</td>
+      <td>
+      <input
+        type="number"
+        min="1"
+        value="${item.cantidad}"
+        class="w-50 pl-2 input-elemento"
+      />
+      </td>
+      <td>
+      <button class="delete btn btn-warning">ELIMINAR</button>
+      </td>
+    `;
+    tbody.append(tr);
+
+    tr.querySelector(".delete").addEventListener("click", removeItemCarrito);
+    tr.querySelector(".input-elemento").addEventListener(
+      "change",
+      sumaCantidad
+    );
+  });
+  CarritoTotal();
+};
+
+const CarritoTotal = () => {
+  let Total = 0;
+  const itemCartTotal = document.querySelector(".itemCartTotal");
+  carrito.forEach((item) => {
+    const precio = Number(item.price);
+    Total = Total + precio * item.cantidad;
+  });
+
+  itemCartTotal.innerHTML = `Total a Pagar $${Total.toLocaleString("de-DE")}`;
+  addLocalStorage();
+};
+
+const removeItemCarrito = (e) => {
+  const buttonDelete = e.target;
+  const tr = buttonDelete.closest(".ItemCarrito");
+  const title = tr.querySelector(".title").textContent;
+  for (let i = 0; i < carrito.length; i++) {
+    if (carrito[i].title.trim() === title.trim()) {
+      carrito.splice(i, 1);
+    }
+  }
+
+  tr.remove();
+  CarritoTotal();
+};
+
+const sumaCantidad = (e) => {
+  const sumaInput = e.target;
+  const tr = sumaInput.closest(".ItemCarrito");
+  const title = tr.querySelector(".title").textContent;
+  carrito.forEach((item) => {
+    if (item.title.trim() === title) {
+      sumaInput.value < 1 ? (sumaInput.value = 1) : sumaInput.value;
+      item.cantidad = sumaInput.value;
+      CarritoTotal();
+    }
+  });
+};
+
+btnPagar.addEventListener("click", () => {
+  $("#productsModal").modal("hide");
+  localStorage.clear();
+  alert("Compra efectuada con éxito. Por favor revise su correo.");
+});
+
+const addLocalStorage = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
